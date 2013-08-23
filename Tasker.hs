019a648d111d7b3169dd3c1c1a384294377a2731
Andreas Bock <bock@andreasbock.dk>
2013-08-23 16:20:18 +0200

    getTime, deleteTask and some haddock comments

--- Tasker.hs
+++ Tasker.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS -Wall #-}
 -- module Main(main) where
-import Types
+import Types -- TODO: organise imports
 import Data.List
 import System.Posix.User
 import System.Directory
@@ -18,19 +18,18 @@ taskIdDb = (++) <$> taskDir <*> pure "id"
 
 -- | The 'main' function starts.
 main :: IO ()
-main = fmap head' getArgs >>= action
-  where head' [] = []
-        head' (x:_) = x
+-- main = fmap head' getArgs >>= action
+main = getArgs >>= action
 
 -- | 'action' returns the appropriate action
 -- | based on what it matches.
-action :: String -> IO ()
-action "-h" = options
-action "-l" = listTasks
-action "-a" = addTask
-action "-d" = deleteTask 1
-action "4"  = putStr ""
-action _    = options
+action :: [String] -> IO () -- TODO: rewrite this, getopt for Haskell?
+action ("-h":_)   = options
+action ("-l":_)   = listTasks
+action ("-a":_)   = addTask
+action ("-d":x:_) = deleteTask (read x :: TaskID)
+action ("4":_)    = putStr ""
+action _    	  = options
 
 -- | 'options' prints the user's options.
 options :: IO ()
@@ -49,12 +48,14 @@ listTasks = taskDb >>= BC.readFile >>= putStrLn . BC.unpack
 addTask :: IO ()
 addTask = Task <$> newId <*> getTime <*> getDesc >>= saveTask
 
-deleteTask :: Int -> IO ()
+-- | 'deleteTask' takes a TaskID and deletes
+-- | the desired entry from the database.
+deleteTask :: TaskID -> IO ()
 deleteTask i = do
-			  db <- fmap (toWords . BC.lines) $ taskDb >>= BC.readFile
-			  let filtered = filter (\x -> i /= (readByteString . head) x) db
-			  let out = map BC.unwords filtered -- concat into list of tasks 
-			  writeDB out
+			    db <- fmap (toWords . BC.lines) $ taskDb >>= BC.readFile
+			    let filtered = filter (\x -> i /= (readByteString . head) x) db
+			    let out = map BC.unwords filtered -- concat into list of tasks 
+			    writeDB out
   where toWords = fmap BC.words
 
 -- | 'writeDB' takes a list of bytestrings and
@@ -71,9 +72,9 @@ saveTask task = do
 				 i <- taskDb
 				 BC.appendFile i $ BC.pack $ show task ++ "\n"
 
--- | 'newId' returns a new id number based on 
+-- | 'newId' returns a new TaskId number based on 
 -- | the current one in the ~/tasker/ids file.
-newId :: IO Int
+newId :: IO TaskID
 newId = do
 		  i <- taskIdDb >>= BC.readFile
 		  let curId = readByteString i :: Int -- get id, increment and save
@@ -81,19 +82,30 @@ newId = do
 		  return curId										
   where incrementId = BC.pack . show . succ
 
+-- | 'getTime' asks the user to supply a due date
+-- | or simply applies a timestamp to the task.
 getTime :: IO Day
 getTime = do
-           putStrLn "Type a due date (empty just adds timestamp):"
-           input <- getLine
-           if "" == input
-           then do
-		 		 time <- getCurrentTime
-				 return $ utctDay time
-           else undefined --parseTime input TODO
--- parseTime :: IO Something
+           putStr "Enter desired due date \"yyyy-mm-dd\" (empty for timestamp): "
+           i <- getLine 
+           if "" == i
+           then getTimestamp
+           else do
+				 case reads i of
+				   [(day,"")] -> return day
+				   _		  -> putStr "Erroneous input!" >> getTime
+
+-- | 'getTimestamp' returns the current date.
+getTimestamp :: IO Day				 
+getTimestamp = getCurrentTime >>= return . utctDay
+
+-- | 'getDesc' is a simple wrapper for BC.getLine.
 getDesc :: IO B.ByteString
 getDesc = putStrLn "Type a brief description of the task:" >> BC.getLine
 
 -- | Helper functions:
 readByteString :: Read a => BC.ByteString -> a
 readByteString = read . BC.unpack
+safeHead :: [[a]] -> [a]
+safeHead [] = []
+safeHead (x:_) = x
